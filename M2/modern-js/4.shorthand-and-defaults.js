27561591b80b64d683a52b897b2c2a242d352d81
TaryneLeach <taryneleach@gmail.com>
2022-03-03 10:05:28 -0500

    Commit

--- M2/modern-js/4.shorthand-and-defaults.js
+++ M2/modern-js/4.shorthand-and-defaults.js
@@ -5,21 +5,28 @@ const first = "Nicole";
 const middle = "Aunapu";
 const last = "Mann";
 
+// create an object with three key value pairs: {first: 'Nicole', middle: 'Aunapu', last: 'Mann'}
+const fullName = {
+  first,
+  middle,
+  last,
+};
+// Object shorthand converts the variable assignment operator "=" into the key/val associator operator ":"
 /**
+ 
  * 2. What are some of the benefits of using object shorthand? Are there any limitations? Write a brief response below.
+- shorter 
+BUT
+you're stuck with the same names
  */
 
 /**
  * 3. The function below expects that sometimes name will not be provided. Update the greet() function so that it uses default parameters.
  */
-function greet(name) {
-  let result = "";
-  if (name) {
-    result = `Welcome to NASA, ${name}!`;
-  } else {
-    result = "Welcome to NASA, space cadet!";
-  }
-  return result;
+// const greet = (name = 'space cadet') => `Welcome to NASA, ${name}!`
+function greet(name = 'space cadet') {
+  return `Welcome to NASA, ${name}!`;
+  
 }
 
 greet("Jessica"); //> "Welcome to NASA, Jessica!"
@@ -27,4 +34,7 @@ greet(); //> "Welcome to NASA, space cadet!"
 
 /**
  * 4. What are some of the benefits of using default parameters? Are there any limitations? Write a brief response below.
+ * removing conditional logic from our function definitions
+ *  but
+ * you have to be careful about default values that are arrays or objects
  */
